c7b8dc927a211274144f50d7403ccf8fc3f455e5
IngaAbdullina <95912864+IngaAbdullina@users.noreply.github.com>
2022-05-05 11:31:13 +0300

    Add files via upload

---
+++ ITMO.ExamAlgorithm/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ITMO.FirstCourseProject.ExamAlgorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 24;
+            int S = 0;
+            int i;
+
+            for (i = 1; i <= n; i++)
+            {
+                Console.WriteLine(i + " Введите число от 1 до 100");
+                int T = int.Parse(Console.ReadLine());
+
+                if(T >= 1 && T <= 100)
+                {
+                    S = S + T;
+                }
+                else
+                {
+                    Console.WriteLine("Введенное число должно быть в диапазоне чисел от 1 до 100");
+                    i--;
+                }
+            }
+            int Mid = S / (i - 1);
+            Console.WriteLine(Mid);
+        }
+    }
+}
